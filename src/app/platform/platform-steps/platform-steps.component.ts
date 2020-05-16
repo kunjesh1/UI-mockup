ba0299814e6a374c2d539d46173b773b1f793714
Kunjesh Kumar <kunjesh.kumar@laureate.net>
2020-05-16 17:57:31 +0530

    UI mockup-v2

--- src/app/platform/platform-steps/platform-steps.component.ts
+++ src/app/platform/platform-steps/platform-steps.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatformStepsComponent implements OnInit {
 
+  taskStep = [
+    'Pick the NLU task',
+    'Upload dataset in specific task-oriented format',
+    'Click the train button',
+    'Our ML workflows trigger the most optimized  ML pipeline for the dataset (in terms of scalability, portability, cost. Training ,Deployment and Serving is all abstracted under the hood ) for the finetuning task',
+    'Generate predictions with an improved REST API'
+  ]
+
+  taskFlow = [
+    'NLU Task',
+    'Upload Dataset',
+    'Train',
+    'Trigger Pipeline',
+    'REST API'
+  ]
+
+  icon = [
+    'g_translate',
+    'bar_chart',
+    'memory',
+    'backup',
+    'code'
+  ]
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
--- src/app/platform/platform.module.ts
+++ src/app/platform/platform.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PlatformRoutingModule } from './platform-routing.module';
 import { PlatformComponent } from './platform.component';
 import { PlatformStepsComponent } from './platform-steps/platform-steps.component';
+import { PlatformDetailsComponent } from './platform-details/platform-details.component';
 
 
 @NgModule({
-  declarations: [PlatformComponent, PlatformStepsComponent],
+  declarations: [PlatformComponent, PlatformStepsComponent, PlatformDetailsComponent],
   imports: [
     CommonModule,
     PlatformRoutingModule
